7df6306fe9ce00009e99c10a271ef39dacbe4b41
gszpecht <g.szpecht@gmail.com>
2020-09-29 16:30:57 +0200

    Add files via upload

---
+++ src/main/java/pl/gps/cake_factory/CakeFactoryNameApplication.java
@@ -0,0 +1,13 @@
+package pl.gps.cake_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeFactoryNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeFactoryNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gps/cake_factory/CakeFactoryNameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gps.cake_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeFactoryNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
